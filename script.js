8ffc4aa95812e7c5897714e43f9602128ba2da49
Gopesh018 <140505822+Gopesh018@users.noreply.github.com>
2024-01-02 18:33:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+gsap.from("#nav-center>img,a,.line",{
+    y:-100,
+    duration:1,
+    delay:0.5,
+    opacity:0,
+});
+
+gsap.from(".content-center>img",{
+    duration:1,
+    delay:0.5,
+    opacity:0,
+    scale:2
+});
